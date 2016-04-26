ea44caf04dc082a12b39078264fc0d5141d81f9d
Johan <johan.davidsen@fjakkarin.com>
2016-04-26 16:11:36 +0200

    Included the AVA test runner in the package.json. Implemented the first stage of DropBox sign in procedure.

--- src/app/stores/UserStore.js
+++ src/app/stores/UserStore.js
@@ -2,23 +2,37 @@ import { Alt } from '../lib';
 import { UserActions} from '../actions';
 
 /**
+ * @class UserStore
+ *
+ *
+ * @since 0.2.1
+ * @author Jóhan Davidsen <johan.davidsen@fjakkarin.com>
  *
  */
 class UserStore {
 
     /**
-     *
+     * @constructor
      */
     constructor() {
         // Bind listeners
         this.bindListeners({
-        });
+                setToken: UserActions.SET_TOKEN
+            }
+        );
 
         // State
         this.accessToken = null;
         this.refreshToken = null;
         this.user = null;
         this.error = null;
+    }
+
+    /**
+     *
+     */
+    setToken( params ){
+        this.accessToken = params.accessToken[0].value;
     }
 }
 
