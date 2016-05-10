6a1b42de2c451a931faa0dec774f359279da5f99
Johan <johan.davidsen@fjakkarin.com>
2016-05-10 14:07:13 +0200

    Moved the callbakc function to a different route.

--- src/app/routes/PageNotFound.jsx
+++ src/app/routes/PageNotFound.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-import { UserActions } from '../actions';
-
 /**
  * @class PageNotFound
  *
  * This page is a catch all page. The page is rendered, when there is no route
- * found for that particular URL. This page also handels a special case, which is
- * callback from the social provider. @see componentDidMount.
+ * found for that particular URL. 
  *
  * @since 0.2.0
  * @author Jóhan Davidsen <johan.davidsen@fjakkarin.com>
@@ -25,34 +22,6 @@ export default class PageNotFound extends React.Component {
     }
 
     /**
-     * @method componentDidMount
-     *
-     * This method checks if there are any params passed to this class. If the
-     * params map to a Social callback, the params are set and the component is
-     * directed back to the application.
-     *
-     */
-    componentDidMount(){
-
-        // There are params passed to this class.
-        if(this.props.params.splat){
-            // Handle params
-            let params =  this.props.params.splat.split("&");
-            // Convert params to object of type key, value.
-            params.forEach( ( element, index, array ) => {
-                let values = element.split("=");
-                element = { key: values[0], value: values[1]};
-                array[index] = element;
-            });
-            console.log(params);
-            // Update store
-            UserActions.setToken(params);
-            // Go to Root.
-            this.context.router.push("/");
-        }
-    }
-
-    /**
      * @method render
      *
      */
@@ -64,9 +33,4 @@ export default class PageNotFound extends React.Component {
             </div>
         )
     }
-}
-// The context is passed to the class, such that it is possible to call the
-// router object.
-PageNotFound.contextTypes = {
-    router: React.PropTypes.object.isRequired
 }
