c796becf0201f0811bce4e4ae3952e23d616bcf9
Jóhan Davidsen <johan.davidsen@fjakkarin.com>
2018-05-06 20:15:52 -0200

    Cleaned up the project

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-"use strict";
+'use strict';
 let express     = require('express');
-let session     = require('express-session');
-let bodyParser  = require('body-parser');
-
-let api         = require('./src/api');
-
 let app         = express();
 
 // Serve client code
 app.use(express.static('./public'));
-
-// Use for Post requests.
-app.use('/api', bodyParser.urlencoded({ extended: true }));
-app.use('/api', bodyParser.json());
-
-// Serve API
-app.use('/api', api);
 
 // Serve
 app.listen(3000, () => {
